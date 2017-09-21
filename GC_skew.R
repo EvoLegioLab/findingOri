c03f6f71e92cc7f036823f7eb40ccaebdbab708a
TeaAmmunet <tea.ammunet@gmail.com>
2017-09-21 17:43:12 +0200

    Some unifying changes, plus going back to original gc3 (alternative commented out, doesn't work)

--- GC_skew.R
+++ GC_skew.R
@@ -1,19 +1,21 @@
-library(seqinr)
-library(ape)
+require(seqinr)
+require(ape)
 #Calculating GC skew from whole genome and genomic 3rd codon positions. 
 #NOTE! Prodigal MUST be installed on the computer and working through bash!
 #-----------------------------Import data-------------------------------------
 #Importing data from binary DNA file or with an accession number
 getfastafiles<-function(accession){
-  myDNAbin<-read.GenBank("accession")
+  myDNAbin<-read.GenBank(accession)
   myFasta<-write.dna(myDNAbin,file ="myFasta.fasta", format = "fasta")
   mySeq<-read.fasta("myFasta.fasta")
+  return(mySeq)
 }
 getDNAbin<-function(mybinfile){
   myFasta<-write.dna(mybinfile,file ="myFasta.fasta", format = "fasta")
   mySeq<-read.fasta("myFasta.fasta")
+  return(mySeq)
 }
-#-----------------------GC calculations----------------------------------------
+#-----------------------GC calculations (functions)----------------------------------------
 #Counting numbers of g and c per window
 #Input to countgc either a fasta file (for whole genome gc), pure sequence list or fasta list and sequence position list (for gc3) 
 countgc<-function(seqfile, seqpos){
@@ -56,55 +58,75 @@ countgc<-function(seqfile, seqpos){
   }
   gccount<-list(cC=unlist(cC,recursive=TRUE), cG=unlist(cG, recursive=TRUE), gc=unlist(G_C,recursive=TRUE),xpos=unlist(x, recursive = TRUE))
 }
-gccount<-countgc(mySeq)
-gccount$cumgc<-cumsum(gccount$gc)
-plot(gccount$xpos, gccount$cumgc, type='l')
 #--------------------------------------GC3 positions and figure------------------------------------
-system('prodigal -i myFasta.fasta -o myGenCoord.fasta -d myGenSeqs.fasta')
-mygenseq<-read.fasta('myGenSeqs.fasta')
-myannot<-sapply(mygenseq,function(x) strsplit(getAnnot(x),'#'))
-annotinfo<- lapply(lapply(myannot,'[', 1:4),function(x) as.integer(x[2:4]))
-positcod<-list()
-negcod<-list()
-seqpos<-list()
-codonseqpos<-list()
-seqpos_neg<-list()
-codonseqpos_neg<-list()
 threes<-function(mydata, myannot){
+  tempposit<-list()
+  tempcod<-list()
+  positcod<-list()
+  tempnegcod<-list()
+  tempcodonseqpos_neg<-list()
+  negcod<-list()
+  seqpos<-list()
+  codonseqpos<-list()
+  codonseqpos_neg<-list()
   #This function gets the third codons and their positions for the strand marked (+1)(or -1) in prodigal
-  #it returns the strand vectors
+  #it returns the 3rd nucleotides and their positions for both strands
   for (i in 1:length(mydata)){
     if (names(mydata[i])==names(myannot[i])){
       mydna<-getSequence(mydata[[i]])
+      seqpos<-c(myannot[[i]][1]:myannot[[i]][2])
+      #print(length(seqpos))
       #getting third nucleotides from sequence on the +1 strand
       if (myannot[[i]][3]==1){
-        #print('plus strand')
-        #positcod list creation and concatenation works, tried with two elements
-        positcod<-c(positcod,mydna[seq(3,length(mydna),3)])
+        #with the foor loop code takes forever and has not looked right
+        #for (j in 1:(length(mydna)/3)){
+         # positcod<-c(positcod,mydna[3*j]) 
         #Setting the positions of the nucleotides to another list
-        seqpos<-c(myannot[[i]][1]:myannot[[i]][2])
+          #codonseqpos<-c(codonseqpos,seqpos[3*j])
+          #print(length(positcod));print(length(codonseqpos))
+        #with this code has worked before
+        positcod<-c(positcod,mydna[seq(3,length(mydna),3)])
         codonseqpos<-c(codonseqpos,seqpos[seq(3,length(seqpos),3)])
+        #}
       } else {
-        #print('minus strand')
         #Getting third nucleotides from sequence on the -1 strand
+        #for (j in 1:(length(mydna)/3)){
+        #  negcod<-c(negcod,mydna[3*j])
+        #  codonseqpos_neg<-c(codonseqpos_neg,seqpos[3*j])
         negcod<-c(negcod,mydna[seq(3,length(mydna),3)])
-        seqpos_neg<-c(myannot[[i]][1]:myannot[[i]][2])
-        codonseqpos_neg<-c(codonseqpos_neg,seqpos_neg[seq(3,length(seqpos_neg),3)])
-        #print(unlist(codonseqpos_neg, recursive=TRUE)) #works!
+        codonseqpos_neg<-c(codonseqpos_neg,seqpos[seq(3,length(seqpos),3)])
+        #}
       }
-    } else {  #If names do not match
+      #If names do not match
+      } else {  
       print('Datasets do not match!')
     }
   }
+  #print(head(positcod))
   positcod<-unlist(positcod,recursive=TRUE)
   codonseqpos<-unlist(codonseqpos, recursive=TRUE)
   negcod<-unlist(negcod, recursive = TRUE)
   codonseqpos_neg<-unlist(codonseqpos_neg, recursive=TRUE)
-  thirds<-list(plusncl=positcod,pluspos=codonseqpos,minusncl=negcod,minuspos=codonseqpos_neg)
+  thirds<-list(plusncl=positcod,pluspos=codonseqpos,minusncl=negcod)
   #thirds<-list(plusncl=positcod,pluspos=codonseqpos)
   return(thirds)
 }
+##-----------------RUnning with input--------------
+#GCskews<-function(accession){
+#change your accession code here
+accession<-"CP011051.1"
+mySeq<-getfastafiles(accession)
+#------------GC-----------------
+gccount<-countgc(mySeq)
+gccount$cumgc<-cumsum(gccount$gc)
+plot(gccount$xpos, gccount$cumgc, type='l')
+#-------------gc3-------------
+system('prodigal -i myFasta.fasta -o myGenCoord.fasta -d myGenSeqs.fasta')
+mygenseq<-read.fasta('myGenSeqs.fasta')
+myAnnot<-sapply(mygenseq,function(x) strsplit(getAnnot(x),'#'))
+annotinfo<- lapply(lapply(myAnnot,'[', 1:4),function(x) as.integer(x[2:4]))
 thirds<-threes(mygenseq,annotinfo)
 gc3s<-countgc(thirds$plusncl, thirds$pluspos)
 gc3s$cumgc<-cumsum(gc3s$gc)
 plot(gc3s$xpos, gc3s$cumgc, type='l')
+#}
