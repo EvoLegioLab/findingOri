46cba9a60c281433ba030ed7adf21be170be2feb
TeaAmmunet <tea.ammunet@gmail.com>
2017-09-22 20:59:09 +0200

    Small fixes, plus added T-A skew and simplegenecount

--- GC_skew.R
+++ GC_skew.R
@@ -26,8 +26,11 @@ countgc<-function(seqfile, seqpos){
   }
   cC<-list()
   cG<-list()
+  cA<-list()
+  cT<-list()
   x<-list()
   G_C<-list()
+  T_A<-list()
   windowlen<-1000
   step<-100
   start<-1
@@ -43,7 +46,10 @@ countgc<-function(seqfile, seqpos){
       tabseq<-table(mydna[start:end])
       cC[i]<-tabseq[['c']]
       cG[i]<-tabseq[['g']]
+      cA[i]<-tabseq[['a']]
+      cT[i]<-tabseq[['t']]
       G_C[i]<-tabseq[['g']]-tabseq[['c']]
+      T_A[i]<-tabseq[['t']]-tabseq[['a']]
       x[i]<-xpos[median(c(start:end))]
       start<-start+step-1
       end<-start+windowlen
@@ -52,13 +58,16 @@ countgc<-function(seqfile, seqpos){
       tabseq<-table(c(mydna[start:length(mydna)],seqfile[1:(end-length(mydna))]))
       cC[i]<-tabseq[['c']]
       cG[i]<-tabseq[['g']]
+      cA[i]<-tabseq[['a']]
+      cT[i]<-tabseq[['t']]
       G_C[i]<-tabseq[['g']]-tabseq[['c']]
+      T_A[i]<-tabseq[['t']]-tabseq[['a']]
       x[i]<-xpos[median(c(start:end))]
       start<-start+step-1
       end<-start+windowlen
     }
   }
-  gccount<-list(cC=unlist(cC,recursive=TRUE), cG=unlist(cG, recursive=TRUE), gc=unlist(G_C,recursive=TRUE),xpos=unlist(x, recursive = TRUE))
+  gccount<-list(cC=unlist(cC,recursive=TRUE), cG=unlist(cG, recursive=TRUE), cT=unlist(cT, recursive=TRUE), cA=unlist(cA, recursive=TRUE),gc=unlist(G_C,recursive=TRUE),ta=unlist(T_A, recursive=TRUE), xpos=unlist(x, recursive = TRUE))
 }
 #--------------------------------------GC3 positions and figure------------------------------------
 #This function first checks that the sequence file and annotation file annotations are compatible. Then for both strands, it goes through the codons
@@ -75,7 +84,7 @@ threes<-function(mydata, myannot){
       seqpos<-c(myannot[[i]][1]:myannot[[i]][2])
       #getting third nucleotides from sequence on the +1 strand
       if (myannot[[i]][3]==1){
-        positcod<-c(positcod,myposdna[seq(3,length(myposdna),3)])
+        positcod<-c(positcod,mydna[seq(3,length(mydna),3)])
         #Setting the positions of the nucleotides to another list
         codonseqpos<-c(codonseqpos,seqpos[seq(3,length(seqpos),3)])
         #}
@@ -101,19 +110,56 @@ threes<-function(mydata, myannot){
 #change your accession code here
 accession<-"CP011051.1"
 mySeq<-getfastafiles(accession)
+#NOTE! Fasta used in the following calls MUST be in the variable mySeq!
 #------------GC-----------------
 #Running gc counts on whole sequence
 gccount<-countgc(mySeq)
 gccount$cumgc<-cumsum(gccount$gc)
+gccount$cumta<-cumsum(gccount$ta)
 plot(gccount$xpos, gccount$cumgc, type='l')
+plot(gccount$xpos, gccount$cumta, type='l')
 #-------------gc3-------------
 #Running gene prediction with prodigal and gc3 counts for 3rd codon positions
 system('prodigal -i myFasta.fasta -o myGenCoord.fasta -d myGenSeqs.fasta')
 mygenseq<-read.fasta('myGenSeqs.f')
 myAnnot<-sapply(mygenseq,function(x) strsplit(getAnnot(x),'#'))
 annotinfo<- lapply(lapply(myAnnot,'[', 1:4),function(x) as.integer(x[2:4]))
-thirds<-threes(mySeq,mygenseq,annotinfo)
+thirds<-threes(mygenseq,annotinfo)
 gc3s<-countgc(thirds$plusncl, thirds$pluspos)
 gc3s$cumgc<-cumsum(gc3s$gc)
+gc3s$cumta<-cumsum(gc3s$ta)
 plot(gc3s$xpos, gc3s$cumgc, type='l')
+plot(gc3s$xpos,gc3s$cumta, type='l')
+ter<-gc3s$xpos[match(max(gc3s$cumgc),gc3s$cumgc)]
+ori<-gc3s$xpos[match(min(gc3s$cumgc),gc3s$cumgc)]
+cat('The origin is located at', ori ,' and the terminus at', ter)
+#---Counting genes------------------
+#getting positions on the leading strand
+simplegenecount<-function(annotations){
+  lespos<-list()
+  laspos<-list()
+  lescount<-0
+  lascount<-0
+  if(ori<ter){
+    lespos<-c(ori:ter)
+    laspos<-c(ter:length(mySeq), 1:ori)
+  }else{
+    lespos<-c(ori:length(mySeq), 1:ter)
+    laspos<-c(ter:ori)
+  }
+  for (i in 1:length(annotations)){
+    if(annotations[[i]][2] %in% lespos){
+      lescount<-lescount+1
+    } else{
+      lascount<-lascount+1
+    }
+  }
+  genecounts<-list(lesc=lescount, lasc=lascount)
+}
+genecount<-simplegenecount(annotinfo)
+genecount
+genebias<-(genecount$lesc/(genecount$lesc+genecount$lasc))*100
+cat(genebias, '% of genes are on the leading strand')
+#more elaborated try with windows and plotting
+
 
